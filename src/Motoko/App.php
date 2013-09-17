b4110f1c10e033b8db7a7d5e2dfdef944362bade
clone1018 <luke@axxim.net>
2013-09-17 16:22:20 -0500

    Adding some passing tests and initial routing framework

--- src/Motoko/App.php
+++ src/Motoko/App.php
@@ -4,4 +4,14 @@ namespace Motoko;
 
 class App {
 
+    protected $router;
+
+    public function setRouter(Router $router) {
+        $this->router = $router;
+    }
+
+    public function getRouter() {
+        return $this->router;
+    }
+
 }
--- tests/Motoko/Tests/AppTest.php
+++ tests/Motoko/Tests/AppTest.php
@@ -3,6 +3,21 @@
 namespace Motoko\Tests;
 
 
+use Motoko\Router;
+
 class AppTest extends AbstractTest {
+
+    public function testAppInitialization() {
+
+    }
+
+    public function testRouterInjection() {
+
+        $router = new Router();
+        $this->app->setRouter($router);
+
+        $this->assertSame($router, $this->app->getRouter());
+
+    }
 
 }
