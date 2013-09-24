12fed23575dd4d68f5114eb99846458ca36b0e30
clone1018 <luke@axxim.net>
2013-09-24 08:58:35 -0500

    Adding in some initial route parsing

--- src/Motoko/Router.php
+++ src/Motoko/Router.php
@@ -5,5 +5,87 @@ namespace Motoko;
 
 class Router {
 
+    public $routes;
+
+    /**
+     * Add router
+     *
+     * @param string $method
+     * @param string $path
+     * @param string $controller
+     * @param string $method
+     */
+    public function add($method = 'GET', $path, $cbOrAction) {
+        if(is_callable($cbOrAction)) {
+            // Callback
+
+            return $this->bindCallback($method, $path, $cbOrAction());
+
+        } else {
+            // Controller / Action
+
+            list($controller, $action) = $this->parseAction($cbOrAction);
+
+            if(!method_exists($controller, $action)) {
+                throw new \BadMethodCallException;
+            }
+
+            return $this->bindAction($method, $path, $controller, $action);
+        }
+
+    }
+
+    /**
+     * Bind a route to a callback
+     *
+     * @param string $method
+     * @param string $path
+     * @param Closure $callback
+     *
+     * @return bool
+     */
+    public function bindCallback($method, $path, $callback) {
+        return false;
+    }
+
+    /**
+     * Bind a route to a controller/action
+     *
+     * @param string $method
+     * @param string $path
+     * @param string $controller
+     * @param string $action
+     *
+     * @return bool
+     */
+    public function bindAction($method, $path, $controller, $action) {
+        return false;
+    }
+
+    /**
+     * Parse an action to find the namespace, class and method of an action.
+     *
+     * @param string $action
+     *
+     * @return array
+     */
+    public function parseAction($action) {
+        $namespace = $class = $method = null;
+
+        $parsed = explode('@', $action);
+        if (strpos($parsed[0],'\\') !== false) {
+            // If we have a namespace
+            $namespaced = explode('\\', $parsed[0]);
+
+            $parsed[0] = array_pop($namespaced);
+            $namespace = implode('\\', $namespaced);
+        }
+        $class = $parsed[0];
+        $method = $parsed[1];
+
+        // namespace,class,method
+        return array($namespace, $class, $method);
+    }
+
 
 }
---
+++ tests/Motoko/Tests/RouterTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Motoko\Tests;
+
+use Motoko\Router;
+
+class RouterTest extends AbstractTest {
+
+    public function testAddRoute() {
+        $router = $this->app->getRouter();
+
+        //$router->add('GET', '/', )
+
+    }
+
+    public function testParsingActions() {
+        $router = $this->app->getRouter();
+
+        // Test non-namespaced controller
+        $this->assertEquals(array(null, 'TestController', 'testAction'), $router->parseAction('TestController@testAction'));
+
+        // Test namespaced controller
+        $this->assertEquals(array('Test\\Namespace', 'TestController', 'testAction'), $router->parseAction('Test\\Namespace\\TestController@testAction'));
+    }
+
+}
